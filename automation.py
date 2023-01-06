2e2bc1dd691b66b2ebf474d2791b740a461b5483
sandarklinton <sandarklinton@gmail.com>
2023-01-06 13:36:48 +0700

    Adding data

--- automation.py
+++ automation.py
@@ -1,3 +1,27 @@
-from selenium.webdriver import Chrome
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import time 
 
-print('it works!')
+driver = webdriver.Chrome()
+driver.set_page_load_timeout(20)
+driver.implicitly_wait(20)
+
+driver.get('https://en-gb.facebook.com/reg/')
+
+driver.find_element(By.XPATH, '//div[@class=\'placeholder\']/following-sibling::input').send_keys('Sandar')
+
+driver.find_element(By.XPATH, '//input[@aria-label=\'Surname\']').send_keys('Klinton')
+
+driver.find_element(By.XPATH, '//input[@aria-label=\'Mobile number or email address\']').send_keys('sandarklinton@gmail.com')
+
+driver.find_element(By.XPATH, '//input[@aria-label=\'Re-enter email address\']').send_keys('sandarklinton@gmail.com')
+
+driver.find_element(By.XPATH, '//input[@aria-label=\'New password\']').send_keys('newpassword')
+
+Select(driver.find_element(By.XPATH, '//select[@aria-label=\'Day\']')).select_by_visible_text('24')
+Select(driver.find_element(By.XPATH, '//select[@aria-label=\'Month\']')).select_by_visible_text('Jan')
+Select(driver.find_element(By.XPATH, '//select[@aria-label=\'Year\']')).select_by_visible_text('1995')
+
+driver.find_element(By.XPATH, '//input[@type=\'radio\'][@value=2]').click()
+time.sleep(10)
